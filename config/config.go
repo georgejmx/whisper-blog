6ab85fd269595210885dae5f9db8e71c77f0d8e7
gm <georgejmx@pm.me>
2022-05-28 23:25:40 +0100

    adding the config package afterall, will have private git branch for production config

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import "os"
+
+var (
+	DB_FILEPATH      string
+	AES_IV           string // must be of length 32
+	AES_SPLICE_INDEX string // must be a string parsable to >=0 and <= 31
+)
+
+/* Sets environment variables ued by program. Will be different for integration
+tests than in production */
+func SetupEnv(isProduction bool) {
+	if isProduction {
+		DB_FILEPATH = "./data/blog_trial.db"
+		AES_IV = "snooping6is9bad0"
+		AES_SPLICE_INDEX = "28"
+	} else {
+		DB_FILEPATH = "./data/blog_test.db"
+		AES_IV = "snooping6is9bad0"
+		AES_SPLICE_INDEX = "28"
+	}
+	os.Setenv("DB_FILEPATH", DB_FILEPATH)
+	os.Setenv("AES_IV", AES_IV)
+	os.Setenv("AES_SPLICE_INDEX", AES_SPLICE_INDEX)
+}
